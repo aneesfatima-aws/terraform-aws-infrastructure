ca3bb001c43b2f077bfec843b6ddbf58d3756e01
aneesfatima-aws <aneesfatima0aws@gmail.com>
2026-06-01 10:10:41 -0700

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,108 @@
+resource "aws_vpc" "lab_vpc" {
+
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "Lab3-VPC"
+  }
+}
+resource "aws_subnet" "public_subnet" {
+
+  vpc_id = aws_vpc.lab_vpc.id
+
+  cidr_block = "10.0.1.0/24"
+
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "Lab3-Public-Subnet"
+  }
+}
+resource "aws_internet_gateway" "igw" {
+
+  vpc_id = aws_vpc.lab_vpc.id
+
+  tags = {
+    Name = "Lab3-IGW"
+  }
+}
+resource "aws_route_table" "public_rt" {
+
+  vpc_id = aws_vpc.lab_vpc.id
+
+  route {
+
+    cidr_block = "0.0.0.0/0"
+
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "Lab3-Route-Table"
+  }
+}
+resource "aws_route_table_association" "rta" {
+
+  subnet_id = aws_subnet.public_subnet.id
+
+  route_table_id = aws_route_table.public_rt.id
+}
+resource "aws_security_group" "web_sg" {
+
+  vpc_id = aws_vpc.lab_vpc.id
+
+  ingress {
+
+    from_port = 22
+
+    to_port = 22
+
+    protocol = "tcp"
+
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+
+    from_port = 80
+
+    to_port = 80
+
+    protocol = "tcp"
+
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+
+    from_port = 0
+
+    to_port = 0
+
+    protocol = "-1"
+
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "Lab3-SG"
+  }
+}
+resource "aws_instance" "web_server" {
+
+  ami = "ami-0c02fb55956c7d316"
+
+  instance_type = "t2.micro"
+
+  subnet_id = aws_subnet.public_subnet.id
+
+  vpc_security_group_ids = [
+    aws_security_group.web_sg.id
+  ]
+
+  key_name = "terraform-key"
+
+  tags = {
+    Name = "Terraform-Web-Server"
+  }
+}
